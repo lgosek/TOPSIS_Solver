d597d1654c5cc6a2d5a7825fa6de428c6d8c75a1
lgosek <luki2012@gmail.com>
2023-03-17 13:56:16 +0100

    init script fix

--- init.sh
+++ init.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
 
-sudo apt install python3-pip
-pip -v install pandas numpy openpyxl
+curl https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+python3 get-pip.py
+
+pip3 install pandas
+pip3 install numpy
+pip3 install openpyxl
